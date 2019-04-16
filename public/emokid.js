eb2522cd9d1e397fddce6b7677917cae82c0feeb
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-04-16 19:22:28 +0300

    PUSH ME TO TOMORROW

--- public/emokid.js
+++ public/emokid.js
@@ -1,12 +1,3 @@
-var left = 101;
-var yipii = new Audio("yipii.mp3")
 function fuu() {
-    if (left <= 1) {
-        yipii.play()
-        document.getElementById('pepe').innerHTML = 'good';
-    }
-    else {
-        left--;
-        document.getElementById('pepe').innerHTML = left;
-    }
+    document.getElementById('nappula').innerHTML = '@';
 }
