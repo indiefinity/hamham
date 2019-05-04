ff391216a6b378870eaef99b7941692391e6eedd
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-05-04 18:08:25 +0300

    a

--- public/emokid.js
+++ public/emokid.js
@@ -3,13 +3,13 @@ guess = 0;
 var oikee;
 oikee = Math.ceil(Math.random * 100);
 function check() {
-    if (document.getElementByID('tonttu').value == oikee) {
+    if (document.getElementById('tonttu').value == oikee) {
         document.getElementById('pepe').innerHTML = 'Good, your guesses: ' + guess;
     }
-    else if (document.getElementByID('tonttu').value > oikee) {
+    else if (document.getElementById('tonttu').value > oikee) {
         document.getElementById('pepe').innerHTML = 'too big';
     }
-    else if (document.getElementByID('tonttu').value < oikee) {
+    else if (document.getElementById('tonttu').value < oikee) {
         document.getElementById('pepe').innerHTML = 'too smell';
     }
 }
