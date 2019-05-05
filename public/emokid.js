9855043d4ffe6f1317173a42788aec4b750f15b3
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-05-05 16:52:23 +0300

    7

--- public/emokid.js
+++ public/emokid.js
@@ -1,17 +1,31 @@
-var guess;
-guess = 0;
-var oikee = 0;
-console.log(oikee);
-oikee = Math.round(Math.random() * 100);
-console.log(oikee);
-function check() {
-    if (document.getElementById('tonttu').value == oikee) {
-        document.getElementById('pepe').innerHTML = 'Good, your guesses: ' + guess;
+var guessScore;
+var guessCorrect;
+function goToMenu() {
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("guess").style.display = "none";
+}
+function startGuess() {
+    guessScore = 0
+    guessCorrect = Math.ceil(Math.random() * 100);
+    document.getElementById("guessPepe").innerHTML = "Veikkaa numero";
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("guess").style.display = "block";
+}
+function guessButton() {
+    if (document.getElementById("guessInput").value == guessCorrect) {
+        document.getElementById("guessPepe").innerHTML = "Hyvä! Arvasit väärin " + guessScore + " kertaa";
+        setTimeout(function() {
+            goToMenu();
+        },2000);
     }
-    else if (document.getElementById('tonttu').value > oikee) {
-        document.getElementById('pepe').innerHTML = 'too big';
+    else if (Number(document.getElementById("guessInput").value) > guessCorrect) {
+        document.getElementById("guessPepe").innerHTML = "pienempi";
     }
-    else if (document.getElementById('tonttu').value < oikee) {
-        document.getElementById('pepe').innerHTML = 'too small';
+    else if (Number(document.getElementById("guessInput").value) < guessCorrect) {
+        document.getElementById("guessPepe").innerHTML = "suurempi";
     }
+    else {
+        document.getElementById("guessPepe").innerHTML = "mitä vittua";
+    }
+    guessScore++;
 }
