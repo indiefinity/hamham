d553319548bc5b0784e7067a3e6d08ffd5b177e4
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-05-06 16:15:44 +0300

    SAumma summaarius

--- public/emokid.js
+++ public/emokid.js
@@ -1,9 +1,15 @@
 var guessScore;
 var guessCorrect;
+
+var mathNum1;
+var mathNum2;
+
 function goToMenu() {
     document.getElementById("menu").style.display = "block";
     document.getElementById("guess").style.display = "none";
+    document.getElementById("math").style.display = "none";
 }
+
 function startGuess() {
     guessScore = 0
     guessCorrect = Math.ceil(Math.random() * 100);
@@ -28,9 +34,28 @@ function guessButton() {
         document.getElementById("guessPepe").innerHTML = "mitä vittua";
     }
     guessScore++;
+
 }
 function startMath() {
-    var mathNum1 = Math.ceil(Math.random() * 10);
-    var mathNum2 = Math.ceil(Math.random() * 10);
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("math").style.display = "block";
+    mathNum1 = Math.ceil(Math.random() * 10);
+    mathNum2 = Math.ceil(Math.random() * 10);
     document.getElementById("mathPepe").innerHTML = mathNum1 + "*" + mathNum2;
+}
+function mathButton() {
+    if (Number(document.getElementById("mathInput").value) == mathNum1 * mathNum2) {
+        mathNum1 = Math.ceil(Math.random() * 10);
+        mathNum2 = Math.ceil(Math.random() * 10);
+        setTimeout(function() {
+            document.getElementById("mathPepe").innerHTML = mathNum1 + "*" + mathNum2;
+        },1000);
+        document.getElementById("mathPepe").innerHTML = "hyvä";
+    }
+    else {
+        setTimeout(function() {
+            document.getElementById("mathPepe").innerHTML = mathNum1 + "*" + mathNum2;
+        },1000);
+        document.getElementById("mathPepe").innerHTML = "oot huono";
+    }
 }
