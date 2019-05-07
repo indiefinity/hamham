649eaf5be66cb0ad3a86505f4736b791f7d154c2
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-05-07 17:08:39 +0300

    44

--- public/emokid.js
+++ public/emokid.js
@@ -1,61 +1,22 @@
-var guessScore;
-var guessCorrect;
-
-var mathNum1;
-var mathNum2;
-
-function goToMenu() {
-    document.getElementById("menu").style.display = "block";
-    document.getElementById("guess").style.display = "none";
-    document.getElementById("math").style.display = "none";
-}
-
-function startGuess() {
-    guessScore = 0
-    guessCorrect = Math.ceil(Math.random() * 100);
-    document.getElementById("guessPepe").innerHTML = "Veikkaa numero";
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("guess").style.display = "block";
-}
-function guessButton() {
-    if (document.getElementById("guessInput").value == guessCorrect) {
-        document.getElementById("guessPepe").innerHTML = "Hyvä! Arvasit väärin " + guessScore + " kertaa";
-        setTimeout(function() {
-            startGuess();
-        },1000);
-    }
-    else if (Number(document.getElementById("guessInput").value) > guessCorrect) {
-        document.getElementById("guessPepe").innerHTML = "pienempi";
-    }
-    else if (Number(document.getElementById("guessInput").value) < guessCorrect) {
-        document.getElementById("guessPepe").innerHTML = "suurempi";
-    }
-    else {
-        document.getElementById("guessPepe").innerHTML = "mitä vittua";
-    }
-    guessScore++;
-
-}
+var num1;
+var num2;
 function startMath() {
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("math").style.display = "block";
-    mathNum1 = Math.ceil(Math.random() * 10);
-    mathNum2 = Math.ceil(Math.random() * 10);
-    document.getElementById("mathPepe").innerHTML = mathNum1 + "*" + mathNum2;
+  num1 = Math.ceil(Math.random() * 10);
+  num2 = Math.ceil(Math.random() * 10);
+  document.getElementById("pepe").innerHTML = (num1 + "*" + num2);
 }
-function mathButton() {
-    if (Number(document.getElementById("mathInput").value) == mathNum1 * mathNum2) {
-        mathNum1 = Math.ceil(Math.random() * 10);
-        mathNum2 = Math.ceil(Math.random() * 10);
-        setTimeout(function() {
-            document.getElementById("mathPepe").innerHTML = mathNum1 + "*" + mathNum2;
-        },500);
-        document.getElementById("mathPepe").innerHTML = "hyvä";
-    }
-    else {
-        setTimeout(function() {
-            document.getElementById("mathPepe").innerHTML = mathNum1 + "*" + mathNum2;
-        },500);
-        document.getElementById("mathPepe").innerHTML = "oot huono";
-    }
-}
+startMath();
+var input = document.getElementById("inp");
+input.addEventListener("keydown", function(event) {
+
+  if (event.keyCode === 13) {
+    event.preventDefault();
+
+    if (Number(input.value) == num1 * num2) {
+      document.getElementById("pepe").innerHTML = "gud";
+      setTimeout(function() {
+      document.getElementById("pepe").innerHTML = num1 + "" + num2;
+    },500);
+
+  }
+});
