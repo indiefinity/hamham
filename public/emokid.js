3e847bf0d3245791cf0dc878deec3f841ef8d0e3
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-05-09 19:03:55 +0300

    5

--- public/emokid.js
+++ public/emokid.js
@@ -1,16 +1,16 @@
 var num1;
 var num2;
 function mathStart() {
-  num1 = Math.ceil(Math.random() * 10);
-  num2 = Math.ceil(Math.random() * 10);
-  document.getElementById("pepe").innerHTML = num1 + "*" + num2;
+  num1 = Math.ceil(Math.random() * 1000);
+  num2 = Math.ceil(Math.random() * 1000);
+  document.getElementById("pepe").innerHTML = num1 + "+" + num2;
 }
 mathStart();
 document.getElementById("inputi")
 .addEventListener("keyup", function(event) {
   event.preventDefault();
   if (event.keyCode === 13) {
-    if (Number(document.getElementById("inputi").value) == num1 * num2) {
+    if (Number(document.getElementById("inputi").value) == num1 + num2) {
       document.getElementById("pepe").innerHTML = "hyvä";
       setTimeout(function() {
         mathStart();
