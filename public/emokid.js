988d7522444695d4b9a70d858c576ebf914cc988
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-07-12 19:45:20 +0300

    3

--- public/emokid.js
+++ public/emokid.js
@@ -1,19 +1,47 @@
+var context ;
+var o;
+var  gain;
+var waveType;
 function fuu() {
-  var context = new AudioContext()
-  var o = context.createOscillator()
-  var  gain= context.createGain()
-  o.connect(gain)
-  gain.connect(context.destination)
-  o.type = "triangle"
-  o.frequency.value = document.getElementById("pepe").value;
-  o.start(0)
-  gain.gain.linearRampToValueAtTime(0, 0.75)
   setTimeout(function() {
     fuu();
   }, 1000)
+  context = new AudioContext();
+  o = context.createOscillator();
+  gain= context.createGain();
+  o.type = waveType;
+  o.connect(gain);
+  gain.connect(context.destination);
+  o.frequency.value = document.getElementById("pepe").value;
+  o.start(0);
+  
+  console.log(o.type)
+  gain.gain.linearRampToValueAtTime(0, 0.25);
 }
 function start() {
-  document.getElementById("nappula").style.display = "none";
+  document.getElementById("sine").style.display = "none";
+  document.getElementById("triangle").style.display = "none";
+  document.getElementById("square").style.display = "none";
+  document.getElementById("sawtooth").style.display = "none";
   document.getElementById("pepe").style.display = "";
-  fuu();
 }
+fuu();
+function sine()  {
+  waveType = "sine";
+  start();
+}
+function triangle()  {
+  waveType = "triangle";
+  start();
+}
+function square()  {
+  waveType = "square";
+  start();
+}
+function sawtooth()  {
+  waveType = "sawtooth";
+  start();
+}
+
+
+
