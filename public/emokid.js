0eb0536dadea7509f7a75204e4a3d50cfc0cf3b0
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-07-13 22:05:42 +0300

    w

--- public/emokid.js
+++ public/emokid.js
@@ -2,34 +2,18 @@ var context = new AudioContext(),
     oscillator,
     waveType;
 
-function playOscillator(time, freq) {
+function play(time, freq, type) {
     oscillator = context.createOscillator();
     oscillator.connect(context.destination);
     oscillator.frequency.value = freq;
-    oscillator.type = waveType;
+    oscillator.type = type;
     oscillator.start(context.currentTime);
     oscillator.stop(context.currentTime + time / 1000);
 }
 function fuu() {
-  document.getElementById("napi").style.display = "none";
-  document.getElementById("pepe").style.display = "";
-  document.getElementById("sine").style.display = "";
-  document.getElementById("triangle").style.display = "";
-  document.getElementById("square").style.display = "";
-  document.getElementById("sawtooth").style.display = "";
-  setInterval(function() {
-    playOscillator(250, document.getElementById("pepe").value);
-  },500);
-}
-function sine() {
-  waveType = "sine";
-}
-function triangle() {
-  waveType = "triangle";
-}
-function square() {
-  waveType = "square";
-}
-function sawtooth() {
-  waveType = "sawtooth";
+    document.getElementById("napi").style.display = "none";
+    setTimeout(function() {
+        fuu();
+    }, document.getElementById("beepFreqBlank").value);
+    play(document.getElementById("beepFreq").value, document.getElementById("freq").value, "square");
 }
