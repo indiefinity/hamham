f0657ce96717e05bcd89779dcfa8822cbb32c352
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2019-08-07 13:12:42 +0300

    Holy shit now youy can drive becoz you arent 7 anymoar and its so frikin cool

--- public/emokid.js
+++ public/emokid.js
@@ -1,40 +1,49 @@
-var ut = new SpeechSynthesisUtterance();
-function joke() {
-    $.ajax({
-        url: 'https://icanhazdadjoke.com/',
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader("Accept", "application/json");
-        }, success: function(data) {
-            console.log(data);
-            document.getElementById("papa").innerHTML = data.joke;
-            ut.text = data.joke + " hahahahahahaha";
-            if (Math.floor(Math.random() * 4) == 0) {
-                ut.text = ut.text + " why aren't you laughing.";
-            };
-            window.speechSynthesis.cancel(ut);
-            window.speechSynthesis.speak(ut);
-        }
-    });
-};
+var carX = 200; 
+var carY = 100;
+var dir = 0;
 
 
-var red = 255;
-var green = 0;
-var blue = 0;
+var c = document.getElementById("peli");
+var ctx = c.getContext("2d");
 
-var fade = setInterval(function(){
-    if (red > 0 && blue == 0) {
-        red--;
-        green++;
+
+var tonks = setInterval(function() {
+
+    c.width = c.width;
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(carX, carY, 10, 10);
+}, 10);
+
+window.addEventListener( "keypress", doKeyDown, false )
+c.addEventListener( "keydown", doKeyDown, true);
+function doKeyDown(e) {
+    console.log(e.keyCode)
+    switch(e.keyCode) {
+        case 100:
+            if (dir == 3) {dir = 0;}
+            else {dir++;}
+            break;
+        case 97:
+            if (dir == 0) {dir = 3;}
+            else {dir--;}
+            break;
+        case 119:
+            switch (dir) {
+                case 0:
+                    carY -= 10;
+                    break;
+                case 1:
+                    carX += 10;
+                    break;
+                case 2:
+                    carY += 10;
+                    break;
+                case 3:
+                    carX -= 10;
+                    break;
+            }
+            break;
     }
-    else if (green > 0) {
-        green--;
-        blue++;
-    }
-    else if (blue > 0) {
-        blue--;
-        red++;
-    };
+}
 
-    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-},10);
+
