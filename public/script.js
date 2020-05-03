07eeb5908002a81e0de19bf5e4f11a0d2d07ba8a
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2020-05-03 21:28:17 +0300

    ms paint is old

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ function setup() {
     for (let i = 0; i < 1000; i++) {
         balls[i] = new Ball();
     }
-};
+}
 
 function draw() {
     background(0);
@@ -12,24 +12,42 @@ function draw() {
         balls[i].move();
         balls[i].show();
     }
-};
+}
 
+function mouseDragged() {
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].click(mouseX, mouseY);
+    }
+} 
 
 class Ball {
     constructor() {
         this.x = random(width);
         this.y = random(height);
-        this.r = random(10, 50);
-    };
+        this.r = random(10, 25);
+        this.a = 10;
+    }
     
+    click(x, y) {
+        let d = dist(x, y, this.x, this.y)
+        if (d < this.r) {
+            this.a = 100;
+        }
+    }
+    
+
     move() {
-        this.x += random(-5, 5);
-        this.y += random(-5, 5);
-    };
+        this.x += random(-2.5, 2.5);
+        this.y += random(-2.5, 2.5);
+    }
 
     show() {
+        if (this.a > 10) {
+            this.a -= 0.25
+        }
+    
         noStroke();
-        fill(255, 10)
-        ellipse(this.x, this.y, this.r);
-    };
+        fill(255, this.a)
+        ellipse(this.x, this.y, this.r*2);
+    }
 }
