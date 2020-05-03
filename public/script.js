b8807ff124c487b0c8819d6f4213f55e827f6596
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2020-05-03 16:54:03 +0300

    gyno x pornoa

--- public/script.js
+++ public/script.js
@@ -7,6 +7,7 @@ function draw() {
     background(0, 0, 0, 6);
     noStroke();
     for (let i = 0; i < 100; i++) {
-        ellipse(map(mouseX, 0, width, i*i, width-(i*i)), (i*5), 25, 25);
+        fill(color(255, 255-map(i, 0, 100, 0, 255), 255-map(i, 0, 100, 0, 255)));
+        ellipse(map(mouseX, 0, width, i*i, width-(i*i)), (i*4), 25, 25);
     };
 };
