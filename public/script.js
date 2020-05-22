b65871b91e04ccf0be8e90408568884c5bd3fc22
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2020-05-22 22:26:31 +0300

    Vittu tää tapa on paskaa

--- public/script.js
+++ public/script.js
@@ -1,79 +1,53 @@
-
-var balls = [];
+let balls = [];
 function setup() {
-    createCanvas(innerWidth, innerHeight);
-    background(0);
-    angleMode(DEGREES);
-    noStroke();
-    
-    
-    for(var i = 0; i < 1000; i++) {
+    createCanvas(600,400);
+    for (let i = 0; i < 1000; i++) {
         balls[i] = new Ball();
     }
-};
+}
 
 function draw() {
-    colorMode(RGB);
-    background(0, 25);
-    
-    for(var i = 0; i < balls.length; i++) {
-        balls[i].update();
-        balls[i].draw();
+    background(0);
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].move();
+        balls[i].show();
     }
-<<<<<<< HEAD
-    
 }
-=======
-};
->>>>>>> parent of 07eeb59... ms paint is old
 
+function mouseDragged() {
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].click(mouseX, mouseY);
+    }
+} 
 
 class Ball {
     constructor() {
         this.x = random(width);
         this.y = random(height);
-<<<<<<< HEAD
-        this.s = random(10,40);
-        this.d = random(360);
-        this.v = random(0,10);
-
-        this.c = [this.r, this.g, this.b];
+        this.r = random(10, 25);
+        this.a = 10;
     }
-
-    update() {
-        this.d += random(-22.5, 22.5);
-        this.x += this.v * sin(this.d);
-        this.y += this.v * cos(this.d);
-        this.v += random(-0.5, 0.5);
-        if (this.x < 0 || this.x > width || this.y < 0 || this.y > height) {
-            this.d += 180;
-        }
-
-
-
-
-    }
-
-    draw() {
-        
-        colorMode(HSB);
-        fill(map(this.v % 10, 0, 10, 0, 255), 255, 255)
-        circle(this.x,this.y,this.s);
-    }
-}
-=======
-        this.r = random(10, 50);
-    };
     
+    click(x, y) {
+        let d = dist(x, y, this.x, this.y)
+        if (d < this.r) {
+            this.a = 100;
+        }
+    }
+    
+
     move() {
-        this.x += random(-5, 5);
-        this.y += random(-5, 5);
-    };
+        this.x += random(-2.5, 2.5);
+        this.y += random(-2.5, 2.5);
+    }
 
     show() {
+        if (this.a > 10) {
+            this.a -= 0.25
+        }
+    
         noStroke();
-        fill(255, 10)
-        ellipse(this.x, this.y, this.r);
-    };
+        fill(255, this.a)
+        ellipse(this.x, this.y, this.r*2);
+    }
 }
->>>>>>> parent of 07eeb59... ms paint is old
