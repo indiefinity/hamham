aa79adabcaef844a8a08918c3bfb118d8a7706d0
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2020-05-23 21:48:57 +0300

    w

--- public/fade.js
+++ public/fade.js
@@ -1,20 +1,20 @@
-var red = 255;
-var green = 0;
-var blue = 0;
+// var red = 255;
+// var green = 0;
+// var blue = 0;
 
-var fade = setInterval(function(){
-    if (red > 0 && blue == 0) {
-        red--;
-        green++;
-    }
-    else if (green > 0) {
-        green--;
-        blue++;
-    }
-    else if (blue > 0) {
-        blue--;
-        red++;
-    };
+// var fade = setInterval(function(){
+//     if (red > 0 && blue == 0) {
+//         red--;
+//         green++;
+//     }
+//     else if (green > 0) {
+//         green--;
+//         blue++;
+//     }
+//     else if (blue > 0) {
+//         blue--;
+//         red++;
+//     };
 
-    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-},10);
+//     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+// },10);
--- public/script.js
+++ public/script.js
@@ -1,53 +1,53 @@
-let balls = [];
-function setup() {
-    createCanvas(600,400);
-    for (let i = 0; i < 1000; i++) {
-        balls[i] = new Ball();
-    }
-}
+// let balls = [];
+// function setup() {
+//     createCanvas(600,400);
+//     for (let i = 0; i < 1000; i++) {
+//         balls[i] = new Ball();
+//     }
+// }
 
-function draw() {
-    background(0);
-    for (let i = 0; i < balls.length; i++) {
-        balls[i].move();
-        balls[i].show();
-    }
-}
+// function draw() {
+//     background(0);
+//     for (let i = 0; i < balls.length; i++) {
+//         balls[i].move();
+//         balls[i].show();
+//     }
+// }
 
-function mouseDragged() {
-    for (let i = 0; i < balls.length; i++) {
-        balls[i].click(mouseX, mouseY);
-    }
-} 
+// function mouseDragged() {
+//     for (let i = 0; i < balls.length; i++) {
+//         balls[i].click(mouseX, mouseY);
+//     }
+// } 
 
-class Ball {
-    constructor() {
-        this.x = random(width);
-        this.y = random(height);
-        this.r = random(10, 25);
-        this.a = 10;
-    }
+// class Ball {
+//     constructor() {
+//         this.x = random(width);
+//         this.y = random(height);
+//         this.r = random(10, 25);
+//         this.a = 10;
+//     }
     
-    click(x, y) {
-        let d = dist(x, y, this.x, this.y)
-        if (d < this.r) {
-            this.a = 100;
-        }
-    }
+//     click(x, y) {
+//         let d = dist(x, y, this.x, this.y)
+//         if (d < this.r) {
+//             this.a = 100;
+//         }
+//     }
     
 
-    move() {
-        this.x += random(-2.5, 2.5);
-        this.y += random(-2.5, 2.5);
-    }
+//     move() {
+//         this.x += random(-2.5, 2.5);
+//         this.y += random(-2.5, 2.5);
+//     }
 
-    show() {
-        if (this.a > 10) {
-            this.a -= 0.25
-        }
+//     show() {
+//         if (this.a > 10) {
+//             this.a -= 0.25
+//         }
     
-        noStroke();
-        fill(255, this.a)
-        ellipse(this.x, this.y, this.r*2);
-    }
-}
+//         noStroke();
+//         fill(255, this.a)
+//         ellipse(this.x, this.y, this.r*2);
+//     }
+// }
