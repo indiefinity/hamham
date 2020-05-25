5d60edc42a9b0abc4bda35d98a6488af77977253
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2020-05-25 21:28:23 +0300

    2112

--- public/script.js
+++ public/script.js
@@ -1,53 +1,40 @@
-// let balls = [];
-// function setup() {
-//     createCanvas(600,400);
-//     for (let i = 0; i < 1000; i++) {
-//         balls[i] = new Ball();
-//     }
-// }
+let balls = [];
+function setup() {
+    createCanvas(600,400);
+    angleMode(DEGREES);
+    for (let i = 0; i < 25; i++) {
+        balls[i] = new Ball();
+    }
+}
 
-// function draw() {
-//     background(0);
-//     for (let i = 0; i < balls.length; i++) {
-//         balls[i].move();
-//         balls[i].show();
-//     }
-// }
+function draw() {
+    background(0);
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].move();
+        balls[i].draw();
+    }
+}
 
-// function mouseDragged() {
-//     for (let i = 0; i < balls.length; i++) {
-//         balls[i].click(mouseX, mouseY);
-//     }
-// } 
+class Ball {
+    constructor() {
+        
+        this.r = 15;
+        this.x = random(this.r, width - this.r);
+        this.y = random(this.r, height - this.r);
+        this.d = random(360);
+    }
+    draw() {
+        noStroke();
+        circle(this.x, this.y, this.r*2);
+    }
+    move() {
+        this.x += cos(this.d);
+        this.y += sin(this.d);
 
-// class Ball {
-//     constructor() {
-//         this.x = random(width);
-//         this.y = random(height);
-//         this.r = random(10, 25);
-//         this.a = 10;
-//     }
-    
-//     click(x, y) {
-//         let d = dist(x, y, this.x, this.y)
-//         if (d < this.r) {
-//             this.a = 100;
-//         }
-//     }
-    
+        if (this.x - this.r < 0 ||
+            this.x + this.r > width) {this.d = 180 - this.d;};
 
-//     move() {
-//         this.x += random(-2.5, 2.5);
-//         this.y += random(-2.5, 2.5);
-//     }
-
-//     show() {
-//         if (this.a > 10) {
-//             this.a -= 0.25
-//         }
-    
-//         noStroke();
-//         fill(255, this.a)
-//         ellipse(this.x, this.y, this.r*2);
-//     }
-// }
+        if (this.y - this.r < 0 ||
+            this.y + this.r > height) {this.d = 360 - this.d;};
+    }
+}
