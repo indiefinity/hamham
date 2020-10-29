1401f386ea160738a896e4a6b57cad812e5390ef
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2020-10-29 23:10:25 +0200

    yes

--- public/script.js
+++ public/script.js
@@ -5,14 +5,26 @@ function setup() {
   for (let i = 0; i < 1000; i++) {
     balls[i] = new Ball();
   }
+  noStroke();
+  colorMode(HSB)
 }
 
 function draw() {
-  background(0);
+  background(0, 240);
   mouse = createVector(mouseX, mouseY);
   for (let i = 0; i < balls.length; i++) {
     balls[i].do();
   }
+}
+
+let mousepressed = false;
+
+function mousePressed() {
+  mousepressed = true;
+}
+
+function mouseReleased() {
+  mousepressed = false;
 }
 
 class Ball {
@@ -22,8 +34,12 @@ class Ball {
   }
 
   do() {
-    let x = p5.Vector.sub(mouse, this.p); //laskee cursorin ja pallon välin
-    this.v.add(x.setMag(1));
+    fill(map(this.v.mag(), 0, 50, 0, 255),255,255);
+    if(mousepressed) {
+      let x = p5.Vector.sub(mouse, this.p); //laskee cursorin ja pallon välin
+      this.v.add(x.setMag(1));
+    }
+    this.v.mult(0.99)
     this.p.add(this.v);
     circle(this.p.x, this.p.y, 10);
   }
