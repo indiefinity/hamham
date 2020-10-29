53f5a048d20ea6fe1dc879f7c084a8968e7f2dbb
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2020-10-29 22:53:39 +0200

    WAT

--- public/script.js
+++ public/script.js
@@ -2,13 +2,13 @@ let balls = [];
 let mouse;
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 1000; i++) {
     balls[i] = new Ball();
   }
 }
 
 function draw() {
-  background(255);
+  background(0);
   mouse = createVector(mouseX, mouseY);
   for (let i = 0; i < balls.length; i++) {
     balls[i].do();
@@ -18,12 +18,14 @@ function draw() {
 class Ball {
   constructor() {
     this.p = createVector(random(width), random(height));
+    this.v = createVector(0,0);
   }
 
   do() {
-    let x = p5.Vector.sub(mouse, this.p);
-    drawArrow(this.p, x.setMag(40), "red");
-    this.p = p5.Vector.add(this.p, x.setMag(20));
+    let x = p5.Vector.sub(mouse, this.p); //laskee cursorin ja pallon välin
+    this.v.add(x.setMag(1));
+    this.p.add(this.v);
+    circle(this.p.x, this.p.y, 10);
   }
 }
 
