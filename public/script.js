0ef0e13384054a9c4f42167480b58df295d1839d
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2020-10-30 12:31:23 +0200

    Tein tän koulus lmao

--- public/script.js
+++ public/script.js
@@ -1,13 +1,21 @@
 let balls = [];
 let mouse;
+let ballCount = 1000;
+
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  for (let i = 0; i < 1000; i++) {
-    balls[i] = new Ball();
-  }
+  makeBalls(ballCount);
   noStroke();
   colorMode(HSB)
 }
+
+function makeBalls(amount) {
+  balls = [];
+  for (let i = 0; i < amount; i++) {
+    balls[i] = new Ball();
+  }
+}
+
 
 function draw() {
   background(0, 240);
@@ -17,17 +25,38 @@ function draw() {
   }
 }
 
+let borders = false;
+
+function keyPressed() {
+  
+  if (keyCode === 66) {
+    if (borders) {
+      console.log("borders off");
+      borders = false;
+    }
+    else {
+      console.log("borders on");
+      borders = true;
+    }
+  }
+  
+  if (keyCode === 38) {
+    ballCount *= 1.25;
+    console.log("ballcount increased");
+    makeBalls(ballCount);
+  }
+  else if (keyCode === 40) {
+    ballCount *= 1 / 1.25;
+    console.log("ballcount decreased");
+    makeBalls(ballCount);
+  }
+}
+
 let mousepressed = false;
 
-function mousePressed() {
-  mousepressed = true;
-}
-
-function mouseReleased() {
-  mousepressed = false;
-}
 
 class Ball {
+  
   constructor() {
     this.p = createVector(random(width), random(height));
     this.v = createVector(0,0);
@@ -35,12 +64,34 @@ class Ball {
 
   do() {
     fill(map(this.v.mag(), 0, 50, 0, 255),255,255);
-    if(mousepressed) {
-      let x = p5.Vector.sub(mouse, this.p); //laskee cursorin ja pallon välin
+    
+    if (borders) {
+      if (this.p.x < 0) {
+        this.p.x = 0;
+        this.v.x *= -1;
+      }
+      else if (this.p.x > width) {
+        this.p.x = width;
+        this.v.x *= -1;
+      }
+      else if (this.p.y < 0) {
+        this.p.y = 0;
+        this.v.y *= -1;
+      }
+      else if (this.p.y > height) {
+        this.p.y = height;
+        this.v.y *= -1;
+      }
+    }
+    
+    if(mouseIsPressed) {
+      let x = p5.Vector.sub(mouse, this.p);
       this.v.add(x.setMag(1));
     }
-    this.v.mult(0.99)
+    
+    this.v.mult(0.99);
     this.p.add(this.v);
+    
     circle(this.p.x, this.p.y, 10);
   }
 }
