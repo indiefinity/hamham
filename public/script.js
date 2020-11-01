5abd73d17f388788f595e9486eff9a51878a8895
Tatuttikana <47392009+Tatuttikana@users.noreply.github.com>
2020-11-01 15:28:52 +0200

    e

--- public/script.js
+++ public/script.js
@@ -4,14 +4,11 @@ let ballCount = 1000;
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  makeBalls(ballCount);
   noStroke();
   colorMode(HSB)
-}
 
-function makeBalls(amount) {
   balls = [];
-  for (let i = 0; i < amount; i++) {
+  for (let i = 0; i < ballCount; i++) {
     balls[i] = new Ball();
   }
 }
@@ -20,52 +17,57 @@ function makeBalls(amount) {
 function draw() {
   background(0, 240);
   mouse = createVector(mouseX, mouseY);
+  
   for (let i = 0; i < balls.length; i++) {
     balls[i].do();
-  }
+  }  
 }
 
 let borders = false;
 let gravity = false;
 
 function keyPressed() {
-  
-  if (keyCode === 66) {
-    if (borders) {
-      console.log("borders off");
-      borders = false;
-    }
-    else {
-      console.log("borders on");
-      borders = true;
-    }
-  }
-  
-  if (keyCode === 38) {
-    ballCount *= 1.25;
-    console.log("ballcount increased");
-    makeBalls(ballCount);
-  }
-  else if (keyCode === 40) {
-    ballCount *= 1 / 1.25;
-    console.log("ballcount decreased");
-    makeBalls(ballCount);
-  }
-  
-  if (keyCode === 71) {
-    if (gravity) {
-      console.log("gravity off");
-      gravity = false;
-    }
-    else {
-      console.log("gravity on");
-      gravity = true;
-    }
-  }
-  
-}
+  switch(keyCode) {
+    case 66:
+      if (borders) {
+        console.log("borders off");
+        borders = false;
+      }
+      else {
+        console.log("borders on");
+        borders = true;
+      }
+      break;
 
-let mousepressed = false;
+    case 38:
+      console.log("ballcount increased");
+      ballCount *= 2;
+      while (balls.length < ballCount) {
+        balls[balls.length] = new Ball();
+      }
+      break;
+
+    case 40:
+      console.log("ballcount decreased");
+      ballCount *= 1 / 2;
+      while (balls.length > ballCount) {
+        delete balls[balls.length - 1];
+        balls.pop();
+      }
+      break;
+
+    case 71:
+      if (gravity) {
+        console.log("gravity off");
+        gravity = false;
+      }
+      else {
+        console.log("gravity on");
+        gravity = true;
+      break;
+    }
+  }
+}
 
 
 class Ball {
@@ -80,19 +82,20 @@ class Ball {
     
     if (borders) {
       if (this.p.x < 0) {
-        this.p.x = 0;
+        console.log(this.v.x)
+        this.p.x = -this.p.x;
         this.v.x *= -1;
       }
       else if (this.p.x > width) {
-        this.p.x = width;
+        this.p.x = width - (width - this.p.x);
         this.v.x *= -1;
       }
       else if (this.p.y < 0) {
-        this.p.y = 0;
+        this.p.y = -this.p.y;
         this.v.y *= -1;
       }
       else if (this.p.y > height) {
-        this.p.y = height;
+        this.p.y = height - (height - this.p.y);
         this.v.y *= -1;
       }
     }
